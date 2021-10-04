f92de95cf88bd9bed5fd01f0850d7ee316a3b740
ILYA <aleksin095@gmail.com>
2021-10-04 19:58:35 +0300

    Section 4

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {
-  View
-} from 'react-native';
+import { View, SafeAreaView } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AppText from './app/components/AppText';
+import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <ListingDetailsScreen />
   );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
 
 function WelcomeScreen() {
     return (
         <ImageBackground
             style={styles.background}
+            blurRadius={10}
             source={require('../assets/background.jpg')}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-                <Text>Sell What You Don't Need</Text>
+                <AppText style={styles.tagline}>Sell What You Don't Need</AppText>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.signUpButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title='login' />
+                <AppButton title='register' color='secondary' />
+            </View>
         </ImageBackground>
     );
 }
@@ -23,10 +29,9 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         alignItems: 'center'
     },
-    loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: '#fc5c65'
+    buttonsContainer: {
+        padding: 20,
+        width: '100%'
     },
     logo: {
         width: 100,
@@ -37,11 +42,11 @@ const styles = StyleSheet.create({
         top: 70,
         alignItems: 'center'
     },
-    signUpButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: '#4ecdc4'
-    },
+    tagline: {
+        fontSize: 25,
+        fontWeight: '600',
+        paddingVertical: 20,
+    }
 })
 
 export default WelcomeScreen;
