39cada7f827952c50b5e8e646389861409fa75f1
ILYA <aleksin095@gmail.com>
2021-10-04 13:36:37 +0300

    Section 3

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Alert,
-  SafeAreaView,
-  Button,
-  Platform,
-  StatusBar,
   View
 } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        color='#5b3c88'
-        title='Нажми на меня!'
-        onPress={() => Alert.prompt('Введите текст...', 'Ваше сообщение', (text) => alert(text))}
-      />
-    </SafeAreaView>
+    <ViewImageScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f1f1f1',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
